8b24fff88c8893c7ad1ce649ce87be8bbbc57c04
Kai Röder <kai.roeder@aeb.com>
2024-03-07 12:52:35 +0100

    chore: upgrade to storybook 8 rc

--- packages/angular-demo/src/app/stories/Button.stories.ts
+++ packages/angular-demo/src/app/stories/Button.stories.ts
@@ -1,52 +1,37 @@
-// also exported from '@storybook/angular' if you can deal with breaking changes in 6.1
-import { Meta, StoryFn } from '@storybook/angular';
+import { Meta, StoryObj } from '@storybook/angular';
 import ButtonComponent from './button.component';
-import Button from './button.component';
 
-// More on default export: https://storybook.js.org/docs/angular/writing-stories/introduction#default-export
 export default {
 	title: 'Example/Button',
 	component: ButtonComponent,
-	// More on argTypes: https://storybook.js.org/docs/angular/api/argtypes
 	argTypes: {
 		backgroundColor: { control: 'color' },
 	},
 } as Meta;
 
-// More on component templates: https://storybook.js.org/docs/angular/writing-stories/introduction#using-args
-const Template: StoryFn<Button> = (args: Button) => ({
-	props: args,
-});
+type Story = StoryObj<ButtonComponent>;
 
-export const Primary = {
-	render: Template,
-
+export const Primary: Story = {
 	args: {
 		primary: true,
 		label: 'Button',
 	},
 };
 
-export const Secondary = {
-	render: Template,
-
+export const Secondary: Story = {
 	args: {
 		label: 'Button',
 	},
 };
 
-export const Large = {
-	render: Template,
-
+export const Large: Story = {
 	args: {
 		size: 'large',
 		label: 'Button',
 	},
 };
 
-export const Small = {
-	render: Template,
-
+export const Small: Story = {
 	args: {
 		size: 'small',
 		label: 'Button',
